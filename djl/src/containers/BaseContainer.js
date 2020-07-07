657630929e226308b11495420469e9b928741c7a
lawrend <douglas_lawrence@mac.com>
2020-07-07 16:56:09 -0700

    added goals to base container

--- djl/src/containers/BaseContainer.js
+++ djl/src/containers/BaseContainer.js
@@ -5,6 +5,13 @@ export default class BaseContainer extends Component {
     return (
       <div>
         <h1>I SAY!</h1>
+        <p>every month i can start from scratch and build up this page. get you better at everything: git, create-react-app, routes, etc.</p>
+        <uo>
+          <li>this month - design for phone then move up to larger screen</li>
+          <li>add random poem generator</li>
+          <li>transfer components</li>
+          <li>ASYNC API CALLS</li>
+        </uo>
       </div>
     );
   }
